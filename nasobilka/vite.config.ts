9dfd6024bb350b6ee6d383ef3133c0db9c516706
matej-dostal <79419590+matej-dostal@users.noreply.github.com>
2023-12-23 18:50:55 +0100

    o

--- nasobilka/vite.config.ts
+++ nasobilka/vite.config.ts
@@ -7,7 +7,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/procvicovani/",
+    base: "/nasobilka/",
     plugins: [
         vue(),
         vueJsx(),
